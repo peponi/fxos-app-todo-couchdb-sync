f9c93e0cda7af58c2986e8aeaa2223a19c810c2d
peponi <mail@peponi.tk>
2016-05-29 16:10:43 +0200

    change priority menu return to string and map this string in the todoModel ro an integer instead

--- app/scripts/models/todoModel.js
+++ app/scripts/models/todoModel.js
@@ -5,13 +5,33 @@ var TodoModel = function() {
     var type = 'todo',
         self = this;
 
+    self.map = {
+        'low': 0,
+        'middle': 1,
+        'hight': 2,
+        'urgent': 3
+    };
+
     self.transaction = (formData) => {
-                /* if date field is emtpy set current date an time */
+        /* if date field is emtpy set current date an time */
         if (formData.date.length <= 0) {
             formData.date = fecha.format(new Date(), 'YYYY-MM-DD');
             formData.time = fecha.format(new Date(), 'hh:mm:ss');
         } else {
             formData.time = '';
+        }
+
+        var prio = formData.prio;
+
+        // map the priority string to a integer value
+        // easier to sort later
+        if(prio === 'middle' || prio === 'hight' || prio === 'urgent') {
+            formData.prio = self.map[prio];
+        // priority will be 0 when prio is 'low' 
+        // or default value has been passed  (priority menu was not used)
+        // or some other crap is in the form data
+        } else {
+            formData.prio = 0;
         }
 
         formData.done = false;
